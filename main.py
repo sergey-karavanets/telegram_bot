8ac553696d6f7a22b4c3c2c984e0327a5022e693
karavanets <dront22rus@mail.ru>
2022-11-01 13:56:11 +0700

    add print with date and BTC price

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def get_data():
     req = requests.get('https://yobit.net/api/3/ticker/btc_usd')
     response = req.json()
     sell_price = response['btc_usd']['sell']
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M")}\nSell BTC price: {sell_price}')
 
 if __name__ == '__main__':
     get_data()
