53e4fa834fa6f7c6bea7b1d747744c9220002200
karavanets <dront22rus@mail.ru>
2022-11-01 13:51:23 +0700

    add variable 'sell_price'

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 def get_data():
     req = requests.get('https://yobit.net/api/3/ticker/btc_usd')
     response = req.json()
+    sell_price = response['btc_usd']['sell']
 
 if __name__ == '__main__':
     get_data()
